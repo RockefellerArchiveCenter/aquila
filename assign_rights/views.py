7bf08983daceda24603cc79e5908c5d42af18f84
bonniegee <bgordon@rockarch.org>
2020-06-24 21:45:30 -0400

    add placeholders for views
    comment out flake8 hook

--- assign_rights/views.py
+++ assign_rights/views.py
@@ -1,3 +1,42 @@
-# from django.shortcuts import render
+from django.shortcuts import render
+from django.views.generic import CreateView, DetailView, ListView, UpdateView
 
-# Create your views here.
+
+def RightsShellsListView(ListView):
+    '''browse and search rights shells'''
+    pass
+
+
+def RightsShellsCreateView(CreateView):
+    '''create rights shells'''
+    pass
+
+
+def RightsShellsDetailView(DetailView):
+    '''view a rights shell'''
+    pass
+
+
+def RightsShellsUpdateView(UpdateView):
+    '''update rights shell'''
+    pass
+
+
+def GroupingsListView(ListView):
+    '''browse and search groupings'''
+    pass
+
+
+def GroupingsCreateView(CreateView):
+    '''create groupings'''
+    pass
+
+
+def GroupingsDetailView(DetailView):
+    '''view a grouping'''
+    pass
+
+
+def GroupingsUpdateView(UpdateView):
+    '''update grouping'''
+    pass
