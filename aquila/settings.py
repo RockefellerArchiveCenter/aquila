bb80d8edefac9cfea57378adacec0d1662aa75e9
bonniegee <bgordon@rockarch.org>
2020-06-24 20:57:05 -0400

    correct name of app

--- aquila/settings.py
+++ aquila/settings.py
@@ -100,7 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-AUTH_USER_MODEL = 'package_bag.User'
+AUTH_USER_MODEL = 'assigh_rights.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
