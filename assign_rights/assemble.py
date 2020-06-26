56d7ef6ca63e52c5c417b833caa339309f0088b7
bonniegee <bgordon@rockarch.org>
2020-06-26 12:02:48 -0400

    add rightsassembler class

---
+++ assign_rights/assemble.py
@@ -0,0 +1,19 @@
+# Receive POST request that contains multiple rights IDs and one date
+# For each rights ID, take date to calculate and return rights json (that looks like what Aurora returns)
+# Send rights json back
+
+
+class RightsAssembler(object):
+    """docstring for RightsCalculator"""
+
+    def retrieve_rights(self):
+        """docstring for retrieve_rights"""
+    pass
+
+    def calculate_dates(self):
+        """docstring for calculate_dates"""
+    pass
+
+    def create_json(self):
+        """docstring for create_json"""
+    pass
