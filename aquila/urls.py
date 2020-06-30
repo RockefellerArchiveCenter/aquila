9317ea65006386959939ebfffc95ab722ab08cb0
Hillel Arnold <helrond@hotmail.com>
2020-06-30 12:32:37 -0400

    add login and logout views, plus one authenticated view

--- aquila/urls.py
+++ aquila/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+
+from assign_rights.views import LoggedInView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', LoginView.as_view(template_name="users/login.html"), name="login"),
+    path('logout/', LogoutView.as_view(next_page="/login"), name="logout"),
+    path('logged-in', LoggedInView.as_view(), name="logged-in")
 ]
