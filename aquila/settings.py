baa4c6e7f41f1c47be79a770c0baa9ac7f8a794d
bonniegee <bgordon@rockarch.org>
2020-07-07 17:47:24 -0400

    add template directory to settings.py

--- aquila/settings.py
+++ aquila/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'aquila.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'assign_rights', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
