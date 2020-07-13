1e16ba1fcc9ad985ec42cbe30d7d6a52aa08fbbc
bonniegee <bgordon@rockarch.org>
2020-07-13 16:16:35 -0400

    add rights forms

--- assign_rights/forms.py
+++ assign_rights/forms.py
@@ -1,10 +1,53 @@
-from django import forms
+from django.forms import ModelForm, inlineformset_factory
 
-from .models import Grouping
+from .models import Grouping, RightsGranted, RightsShell
 
 
-class GroupingForm(forms.ModelForm):
+class GroupingForm(ModelForm):
     """docstring for RightsShellForm"""
     class Meta:
         model = Grouping
         fields = ["title", "description", "rights_shells"]
+
+
+class RightsShellForm(ModelForm):
+    """docstring for RightsShellForm"""
+    class Meta:
+        model = RightsShell
+        fields = [
+            'rights_basis',
+            'copyright_status',
+            'determination_date',
+            'note',
+            'applicable_start_date',
+            'applicable_end_date',
+            'start_date_period',
+            'end_date_period',
+            'end_date_open',
+            'license_terms',
+            'statute_citation'
+        ]
+
+
+class RightsGrantedForm(ModelForm):
+    """docstring for RightsShellForm"""
+    class Meta:
+        model = RightsGranted
+        fields = [
+            'basis',
+            'act',
+            'note',
+            'start_date',
+            'end_date',
+            'start_date_period',
+            'end_date_period',
+            'end_date_open',
+        ]
+
+
+RightsGrantedFormSet = inlineformset_factory(
+    RightsShell,
+    RightsGranted,
+    extra=1,
+    form=RightsGrantedForm,
+)
