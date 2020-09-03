d9dc16c1f534c3c8c7286ab83a1de1e644bf4a05
bonniegee <bgordon@rockarch.org>
2020-09-03 10:51:54 -0400

    fix linting

--- setup_user.py
+++ setup_user.py
@@ -3,4 +3,3 @@ from assign_rights.models import User
 if User.objects.filter(username="test").exists():
     print("Creating user")
     User.objects.create_user("test", "test@example.com", "testpassword")
-
