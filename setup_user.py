145b911555f8c3c88ad9807c3a9b172f5438ab90
bonniegee <bgordon@rockarch.org>
2020-09-03 10:25:53 -0400

    better if statement

--- setup_user.py
+++ setup_user.py
@@ -1,6 +1,6 @@
 from assign_rights.models import User
 
-if len(User.objects.all()) == 0:
+if User.objects.filter(username="test").exists():
     print("Creating user")
     User.objects.create_user("test", "test@example.com", "testpassword")
 
