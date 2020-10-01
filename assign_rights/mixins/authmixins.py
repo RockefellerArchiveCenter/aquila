a9d76bdf89e6343dd7324e7d032db563021acc26
Patrick <pgalligan@rockarch.org>
2020-10-01 13:39:47 -0400

    Remove UserMixin
    Removes the UserMixin because it's not doing anything right now.

--- assign_rights/mixins/authmixins.py
+++ assign_rights/mixins/authmixins.py
@@ -7,24 +7,13 @@ class LoggedInMixinDefaults(LoginRequiredMixin):
     login_url = reverse_lazy("login")
 
 
-class UserMixin(LoggedInMixinDefaults, UserPassesTestMixin):
-    """Overwrites the mixin defaults"""
-    authenticated_redirect_url = reverse_lazy("login")
-
-    def test_func(self):
-        """Checks whether a user is a superuser in the application"""
-        if self.request.user.is_superuser:
-            return True
-        return False
-
-
-class EditMixin(UserMixin, UserPassesTestMixin):
+class EditMixin(LoggedInMixinDefaults, UserPassesTestMixin):
     """Checks whether a user is in the edit group"""
     def test_func(self):
         return self.request.user.groups.filter(name='edit').exists()
 
 
-class DeleteMixin(UserMixin, UserPassesTestMixin):
+class DeleteMixin(LoggedInMixinDefaults, UserPassesTestMixin):
     """Checks whether a user is in the delete group"""
     def test_func(self):
         return self.request.user.groups.filter(name='delete').exists()
