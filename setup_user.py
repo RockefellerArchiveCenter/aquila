599ee7d0bfdb7426da69b064ac75aa931168f31b
bonniegee <bgordon@rockarch.org>
2020-10-07 15:05:35 -0400

    add initial user to edit group

--- setup_user.py
+++ setup_user.py
@@ -1,5 +1,9 @@
 from assign_rights.models import User
+from django.contrib.auth.models import Group
 
 if not User.objects.filter(username="test").exists():
     print("Creating user")
     User.objects.create_user("test", "test@example.com", "testpassword")
+    user = User.objects.get(username='test')
+    group = Group.objects.get(name='edit')
+    user.groups.add(group)
