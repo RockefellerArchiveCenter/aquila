09c71d3af0fae975f7fd4a2863d7258e9f45a233
bonniegee <bgordon@rockarch.org>
2021-02-22 14:32:20 -0500

    add donor and policy to edit page

--- assign_rights/views.py
+++ assign_rights/views.py
@@ -96,6 +96,10 @@ class RightsShellUpdateView(PageTitleMixin, EditMixin, UpdateView):
             form_cls = StatuteForm
         elif rights_basis == "License":
             form_cls = LicenseForm
+        elif rights_basis == "Policy":
+            form_cls = OtherForm
+        elif rights_basis == "Donor":
+            form_cls = OtherForm
         else:
             form_cls = OtherForm
         return form_cls
