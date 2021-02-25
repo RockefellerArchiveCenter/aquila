a4e513a7a09da08f56300127ca6daf5c5df640f1
Hillel Arnold <helrond@hotmail.com>
2021-02-25 11:18:21 -0500

    add test user to delete group

--- setup_user.py
+++ setup_user.py
@@ -5,5 +5,5 @@ if not User.objects.filter(username="test").exists():
     print("Creating user")
     User.objects.create_user("test", "test@example.com", "testpassword")
     user = User.objects.get(username='test')
-    group = Group.objects.get(name='edit')
-    user.groups.add(group)
+    groups = Group.objects.filter(name__in=['edit', 'delete'])
+    user.groups.add(*groups)
