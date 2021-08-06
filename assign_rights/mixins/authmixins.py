6895ac5d657f3414afa65b98cd37a467d8c21548
Hillel Arnold <helrond@hotmail.com>
2021-08-06 13:04:51 -0400

    let superusers do stuff

--- assign_rights/mixins/authmixins.py
+++ assign_rights/mixins/authmixins.py
@@ -11,11 +11,17 @@ class EditMixin(LoggedInMixinDefaults, UserPassesTestMixin):
     """Checks whether a user is in the edit group"""
 
     def test_func(self):
-        return self.request.user.groups.filter(name='edit').exists()
+        return any([
+            self.request.user.is_superuser,
+            self.request.user.groups.filter(name='edit').exists()
+        ])
 
 
 class DeleteMixin(LoggedInMixinDefaults, UserPassesTestMixin):
     """Checks whether a user is in the delete group"""
 
     def test_func(self):
-        return self.request.user.groups.filter(name='delete').exists()
+        return any([
+            self.request.user.is_superuser,
+            self.request.user.groups.filter(name='delete').exists()
+        ])
