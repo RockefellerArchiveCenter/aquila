9ef571d75aeedde52bc4f1908ed5871d1da5e850
Hannah <hsistrunk@gmail.com>
2024-03-22 08:42:00 -0400

    add nav toggle js and cleanup nav dropdown

---
+++ assign_rights/static/js/nav-toggle.js
@@ -0,0 +1,15 @@
+// Mobile navigation toggle
+
+document.getElementById('nav-toggle').addEventListener('click', function() {
+  const expandedCurrent = this.getAttribute('aria-expanded')
+
+  // Sets classes and attributes on the nav-toggle
+  this.classList.toggle('active')
+  this.classList.toggle('closed')
+  this.setAttribute('aria-expanded', expandedCurrent === 'true' ? 'false' : 'true')
+
+  // Sets classes and attributes on the nav-toggle-menu
+  menu = document.getElementById('nav-toggle-menu')
+  menu.classList.toggle('active')
+  menu.classList.toggle('closed')
+})
