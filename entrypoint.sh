8ca2816db513449d8b141b89e0ad056f0383d43e
Hillel Arnold <hillel.arnold@gmail.com>
2025-11-20 17:00:57 -0500

    updating infrastructure

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,6 @@
 echo "Apply database migrations"
 ./wait-for-it.sh db:5432 -- python manage.py migrate
 
-# Create initial organizations and users
-echo "Create initial user"
-python manage.py shell < ./setup_user.py
-
-#Start server
+# Start server
 echo "Starting server"
 python manage.py runserver 0.0.0.0:${APPLICATION_PORT}
